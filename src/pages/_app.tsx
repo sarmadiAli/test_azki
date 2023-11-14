1fdab387daa3be2cde95039800f2ab9dceecf195
a.sarmadi <dev.alisarmadi@gmail.com>
2023-11-15 00:35:31 +0330

    add and config: MUI,emotion(theme,serverSideRender,rtl,emotionCatch) & fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import createEmotionCache from 'src/createEmotionCache';
+import type { AppProps } from 'next/app';
+import { CacheProvider, ThemeProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CssBaseline } from '@mui/material';
+import './../styles/globals.css';
+import makeTheme from 'src/styles/makeTheme';
+
+const clientSideEmotionCache = createEmotionCache();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { theme } = makeTheme();
+
+  const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [rtlPlugin],
+  });
+  return (
+    <CacheProvider value={{ ...clientSideEmotionCache, ...cacheRtl }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
